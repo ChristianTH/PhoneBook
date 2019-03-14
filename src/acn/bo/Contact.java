bcf14c0ed40e64f4da109868b947dbf342b6a375
hartono <c.a.hartono@gmail.com>
2019-03-14 08:52:38 +0100

    Update contact
    Added properties name, address and phonenumber

--- src/acn/bo/Contact.java
+++ src/acn/bo/Contact.java
@@ -2,4 +2,32 @@ package acn.bo;
 
 public class Contact {
 
+	private String name;
+	private String address;
+	private String phonenumber;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhonenumber() {
+		return phonenumber;
+	}
+
+	public void setPhonenumber(String phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+
 }
